4bbefbfe9307a97d12bb5c527efbeec9341fc3b4
程杰柱 <jack-cheng@chengjizhudeMBP.lan>
2017-12-12 22:43:11 +0800

    complete all meeting actions

--- service/entities/meeting.go
+++ service/entities/meeting.go
@@ -14,13 +14,17 @@ type Meeting struct {
 	Sponsor       string    `xorm:"sponsor"`
 }
 
+// ---------------------
+// meeting actions
+// ---------------------
+
 // NewMeeting create a new meeting and add to database
 func NewMeeting(title, start, end, spon string, parts []string) (Meeting, error) {
 	newM := Meeting{}
 	if !(validateTitle(title)) {
 		return newM, errors.New("title has existed")
 	}
-	if !(validateParticipators(parts, title)) {
+	if !(validateParticipators(parts)) {
 		return newM, errors.New("not registered participators")
 	}
 	startTime, ok1 := getTime(start)
@@ -58,6 +62,88 @@ func NewMeeting(title, start, end, spon string, parts []string) (Meeting, error)
 	return newM, nil
 }
 
+// QuitMeeting quit the meeting the user participated in
+func QuitMeeting(user, title string) error {
+	var u User
+	var m Meeting
+
+	agendaDB.Id(user).Get(&u)
+	got, err := agendaDB.Id(title).Get(&m)
+	if !got {
+		return errors.New("title error: no such meeting title")
+	}
+	if err != nil {
+		return err
+	}
+
+	isDel := false
+	for index, t := range u.Meetings {
+		if t == title {
+			u.Meetings = append(u.Meetings[:index], u.Meetings[index+1:]...)
+			isDel = true
+			break
+		}
+	}
+	if !isDel {
+		return errors.New("user error: the user has not participated in the meeting")
+	}
+	for index, t := range m.Participators {
+		if t == user {
+			m.Participators = append(m.Participators[:index], m.Participators[index+1:]...)
+			break
+		}
+	}
+
+	agendaDB.Id(user).Update(&u)
+	if len(m.Participators) == 0 {
+		agendaDB.Id(title).Delete(&m)
+	} else {
+		agendaDB.Id(title).Update(&m)
+	}
+
+	return nil
+}
+
+// ClearMeeting clear all the meeting one sponsored
+func ClearMeeting(user string) {
+	var u User
+	var m Meeting
+
+	agendaDB.Id(user).Get(&u)
+	for _, t := range u.Meetings {
+		agendaDB.Id(t).Get(&m)
+		if m.Sponsor == user {
+			agendaDB.Id(t).Delete(&m)
+		}
+	}
+}
+
+// QueryMeeting query one's meetings between a specified time interval
+func QueryMeeting(user, start, end string) ([]Meeting, error) {
+	var u User
+	var m Meeting
+	var rst []Meeting
+	startTime, ok1 := getTime(start)
+	endTime, ok2 := getTime(end)
+
+	if (!ok1) || (!ok2) {
+		return []Meeting{}, errors.New("time format error: require format \"YYYY-MM-DD\"")
+	}
+	if !(validateTime(startTime, endTime)) {
+		return []Meeting{}, errors.New("start time must before end time")
+	}
+
+	agendaDB.Id(user).Get(&u)
+	for _, t := range u.Meetings {
+		agendaDB.Id(t).Get(&m)
+		if !(m.StartTime.After(endTime) || m.EndTime.Before(startTime)) {
+			rst = append(rst, m)
+		}
+	}
+
+	return rst, nil
+}
+
 // -------------------------
 // Validation Functions
 // -------------------------
@@ -68,10 +154,10 @@ func validateTitle(title string) bool {
 	return !got
 }
 
-func validateParticipators(parts []string, title string) bool {
+func validateParticipators(parts []string) bool {
 	var user User
 	for _, part := range parts {
-		got, _ := agendaDB.Id(title).Get(&user)
+		got, _ := agendaDB.Id(part).Get(&user)
 		if !got {
 			return false
 		}
--- service/service/meeting_handler.go
+++ service/service/meeting_handler.go
@@ -52,3 +52,93 @@ func createMeetingHandler(formatter *render.Render) http.HandlerFunc {
 		formatter.JSON(w, http.StatusCreated, newMeeting)
 	}
 }
+
+func quitMeetingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		req.ParseForm()
+
+		// deal with bad request
+		if len(req.Form["key"][0]) == 0 {
+			formatter.JSON(w, http.StatusBadRequest, struct{ ErrorIndo string }{"key required!"})
+			return
+		}
+		if len(req.Form["title"][0]) == 0 {
+			formatter.JSON(w, http.StatusBadRequest, struct{ ErrorIndo string }{"title required!"})
+			return
+		}
+
+		// check if key is valid
+		currentUser, err := entities.CheckKey(req.Form["key"][0])
+		if err != nil {
+			formatter.JSON(w, http.StatusUnauthorized, struct{ ErrorIndo string }{err.Error()})
+			return
+		}
+
+		// query a meeting
+		err = entities.QuitMeeting(currentUser, req.Form["title"][0])
+		if err != nil {
+			formatter.JSON(w, http.StatusNotFound, struct{ ErrorIndo string }{err.Error()})
+			return
+		}
+
+		formatter.JSON(w, http.StatusBadRequest, struct{ ErrorIndo string }{"success!"})
+	}
+}
+
+func clearMeetingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		req.ParseForm()
+
+		// deal with bad request
+		if len(req.Form["key"][0]) == 0 {
+			formatter.JSON(w, http.StatusBadRequest, struct{ ErrorIndo string }{"key required!"})
+			return
+		}
+
+		// check if key is valid
+		currentUser, err := entities.CheckKey(req.Form["key"][0])
+		if err != nil {
+			formatter.JSON(w, http.StatusUnauthorized, struct{ ErrorIndo string }{err.Error()})
+			return
+		}
+
+		// query a meeting
+		entities.ClearMeeting(currentUser)
+		formatter.JSON(w, http.StatusBadRequest, struct{ ErrorIndo string }{"success!"})
+	}
+}
+
+func queryMeetingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		req.ParseForm()
+
+		// deal with bad request
+		if len(req.Form["key"][0]) == 0 {
+			formatter.JSON(w, http.StatusBadRequest, struct{ ErrorIndo string }{"key required!"})
+			return
+		}
+		if len(req.Form["startTime"][0]) == 0 {
+			formatter.JSON(w, http.StatusBadRequest, struct{ ErrorIndo string }{"startTime required!"})
+			return
+		}
+		if len(req.Form["endTime"][0]) == 0 {
+			formatter.JSON(w, http.StatusBadRequest, struct{ ErrorIndo string }{"endTime required!"})
+			return
+		}
+
+		// check if key is valid
+		currentUser, err := entities.CheckKey(req.Form["key"][0])
+		if err != nil {
+			formatter.JSON(w, http.StatusUnauthorized, struct{ ErrorIndo string }{err.Error()})
+			return
+		}
+
+		newMeeting, err := entities.QueryMeeting(currentUser, req.Form["startTime"][0], req.Form["endTime"][0])
+		if err != nil {
+			formatter.JSON(w, http.StatusNotFound, struct{ ErrorIndo string }{err.Error()})
+			return
+		}
+
+		formatter.JSON(w, http.StatusBadRequest, newMeeting)
+	}
+}
